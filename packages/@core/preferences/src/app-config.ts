78904d98ad0b0c58701d4a23a5e652b6635d1298
Luke Boxwell <130121308+catdddmouse@users.noreply.github.com>
2025-09-08 15:07:35 +0800

    refactor(yudao): 更新网站 logo 并调整应用配置
    - 更新 admin 和 web 应用的 logo 地址
    - 移除 admin 应用的默认首页路径
    - 删除了多个不再使用的应用配置
    - 更新 pdfmake 版本
    - 删除心理学模块的评估任务更新测试代码

--- packages/@core/preferences/src/app-config.ts
+++ packages/@core/preferences/src/app-config.ts
@@ -16,7 +16,7 @@ const appConfigs: Record<string, AppConfig> = {
   admin: {
     name: 'Admin管理后台',
     defaultHomePath: '/analytics',
-    logo: 'https://6d65-mentor-3gyob3y3bdbc2bdb-1305613707.tcb.qcloud.la/static/logo/lvye_logo.png',
+    logo: 'https://6d65-mentor-3gyob3y3bdbc2bdb-1305613707.tcb.qcloud.la/lvye/logo.png',
     theme: {
       primaryColor: 'hsl(144 57% 58%)',
       mode: 'dark',
@@ -25,36 +25,12 @@ const appConfigs: Record<string, AppConfig> = {
   web: {
     name: 'Web前台',
     defaultHomePath: '/home',
-    logo: 'https://6d65-mentor-3gyob3y3bdbc2bdb-1305613707.tcb.qcloud.la/static/logo/lvye_logo.png',
+    logo: 'https://6d65-mentor-3gyob3y3bdbc2bdb-1305613707.tcb.qcloud.la/lvye/logo.png',
     theme: {
       primaryColor: 'hsl(144 57% 58%)',
       mode: 'light',
     },
-  },
-  'web-antd': {
-    name: 'Antd版本',
-    defaultHomePath: '/home',
-    theme: {
-      primaryColor: 'hsl(348 100% 61%)',
-      mode: 'light',
-    },
-  },
-  'web-ele': {
-    name: 'Element版本',
-    defaultHomePath: '/index',
-    theme: {
-      primaryColor: 'hsl(42 84% 61%)',
-      mode: 'light',
-    },
-  },
-  'web-naive': {
-    name: 'Naive版本',
-    defaultHomePath: '/welcome',
-    theme: {
-      primaryColor: 'hsl(212 100% 45%)',
-      mode: 'dark',
-    },
-  },
+  }
 };
 
 // 获取当前应用类型
